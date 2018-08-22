3ec169411ea8798394391fa50f87b103a4eb4b1f
swarnadee bapat <swarnadee.bapat@reancloud.com>
2018-08-22 11:53:02 +0530

    Added the metadata lambda function changes

--- webportal/lambda/add_metadata.py
+++ webportal/lambda/add_metadata.py
@@ -1,18 +1,29 @@
 import boto3
 import logging
-
+import os
 
 def lambda_handler(event, context):
     bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
     s3_object_key = event["Records"][0]["s3"]["object"]["key"]
-    s3 = boto3.resource('s3')
+    add_metadata = 0
+    print("S3 object key:"+s3_object_key)
     logger = logging.getLogger()
     logger.setLevel("INFO")
-    response = s3.meta.client.get_object(Bucket=bucket_name, Key=s3_object_key)
-    if response["Metadata"]:
-        logger.info("This object has been uploaded from web portal")
+    logger.info("EVENT:"+str(s3_object_key))
+    name_of_export_requests_folder = os.environ["EXPORT_REQUEST_FOLDER"]
+    extensions = os.environ["FILE_EXTENSIONS"].split(",")
+    for extension in extensions:
+        if extension in s3_object_key:
+            add_metadata = 1
+    if name_of_export_requests_folder in s3_object_key and add_metadata:
+        s3 = boto3.resource('s3')
+        response = s3.meta.client.get_object(Bucket=bucket_name, Key=s3_object_key)
+        if response["Metadata"]:
+            logger.info("This object has been uploaded from web portal")
+        else:
+            s3_object = s3.Object(bucket_name, s3_object_key)
+            s3_object.metadata.update({'download':'false','export':'true','publish':'false'})
+            s3_object.copy_from(CopySource={'Bucket': bucket_name, 'Key': s3_object_key}, Metadata=s3_object.metadata, MetadataDirective='REPLACE')
+            logger.info("Adding metadata to S3 object has been completed successfully")
     else:
-        s3_object = s3.Object(bucket_name, s3_object_key)
-        s3_object.metadata.update({'download':'false','export':'true','publish':'false'})
-        s3_object.copy_from(CopySource={'Bucket': bucket_name, 'Key': s3_object_key}, Metadata=s3_object.metadata, MetadataDirective='REPLACE')
-        logger.info("Adding metadata to S3 object has been completed successfully")
+        logger.info("Ignored this file with key:"+s3_object_key)
