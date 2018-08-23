31547ec479d0906e8f32d44d66de84a9a37d7807
swarnadee bapat <swarnadee.bapat@reancloud.com>
2018-08-23 16:25:08 +0530

    Fixed UI content

--- webportal/webapp/src/app/account/datasets/datasets.component.ts
+++ webportal/webapp/src/app/account/datasets/datasets.component.ts
@@ -130,7 +130,7 @@ export class DatasetsComponent implements OnInit {
     requestExport(BucketName, mailType, datasetName) {
         const dialogRef = this.dialog.open(DialogBoxComponent, {
             width: '700px',
-            height: '550px',
+            height: '630px',
             data: { userBucketName: this.userBucketName, mailType: mailType, datasetName: datasetName }
         });
 
