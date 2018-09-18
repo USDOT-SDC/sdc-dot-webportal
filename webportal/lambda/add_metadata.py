4c46729b2f5d981b86e05357aa27a437f45bb180
Swarnadee <swarnadee.bapat@reancloud.com>
2018-09-18 12:20:53 -0400

    Metadata changes for export_requests from workstation

--- webportal/lambda/add_metadata.py
+++ webportal/lambda/add_metadata.py
@@ -23,12 +23,13 @@ def lambda_handler(event, context):
         s3 = boto3.resource('s3')
         response = s3.meta.client.get_object(Bucket=bucket_name, Key=s3_object_key)
         if response["Metadata"]:
-            logger.info("This object has been uploaded from web portal")
-        else:
-            s3_object = s3.Object(bucket_name, s3_object_key)
-            s3_object.metadata.update({'download':'false','export':'true','publish':'false'})
-            s3_object.copy_from(CopySource={'Bucket': bucket_name, 'Key': s3_object_key}, Metadata=s3_object.metadata, MetadataDirective='REPLACE')
-            logger.info("Adding metadata to S3 object has been completed successfully")
+            if "download" in response["Metadata"]:
+                logger.info("This object has been uploaded from web portal")
+            else:
+                s3_object = s3.Object(bucket_name, s3_object_key)
+                s3_object.metadata.update({'download':'false','export':'true','publish':'false'})
+                s3_object.copy_from(CopySource={'Bucket': bucket_name, 'Key': s3_object_key}, Metadata=s3_object.metadata, MetadataDirective='REPLACE')
+                logger.info("Adding metadata to S3 object has been completed successfully")
     else:
         logger.info("Ignored this file with key:"+s3_object_key)
     
