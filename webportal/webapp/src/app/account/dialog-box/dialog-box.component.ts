fae256274b41b37aaf6adac83c1b7b05b49d4f5b
Santosh Karla <santosh.karla@gmail.com>
2019-06-27 15:42:24 -0400

    Add modify instance lambda

--- webportal/webapp/src/app/account/dialog-box/dialog-box.component.ts
+++ webportal/webapp/src/app/account/dialog-box/dialog-box.component.ts
@@ -356,7 +356,7 @@ export class DialogBoxComponent implements OnInit {
     }
 
     getTransformedPrice(cost) {
-       return ('$' + (parseFloat(cost)).toFixed(4) + ' per Hour');
+       return ('$' + (parseFloat(cost)).toFixed(2) + ' per Hour');
     }
 
     getTransformedMemory(memory) {
