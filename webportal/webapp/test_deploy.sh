062cb1b108ee0cc50ec4921e25fedac12ea59f4b
Giridhar Raju Jagathapurao <giridhar.jagathapurao@hitachivantara.com>
2019-08-08 15:28:25 -0400

    update hsts changes

--- webportal/webapp/test_deploy.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-ng build --aot
-#upload files
-s3cmd --exclude '.git/*' sync --delete-removed ./dist/ s3://test-dot-sdc-webportal-hosting/
-#set content type of css files
-s3cmd --recursive modify --add-header='content-type':'text/css' --exclude '' --include '.css' s3://test-dot-sdc-webportal-hosting/
-#make everything public
-s3cmd setacl s3://test-dot-sdc-webportal-hosting --acl-public --recursive
