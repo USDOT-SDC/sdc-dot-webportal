d3a49711acea347795d29f78c14868bdeb37fa4f
Cheryl Rousseau <crousseau@acrotron.com>
2019-09-02 11:51:33 -0400

    Add unit tests for 100% code coverage

--- webportal/webapp/src/app/account/account.component.spec.ts
+++ webportal/webapp/src/app/account/account.component.spec.ts
@@ -1,162 +1,115 @@
-import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AccountComponent } from './account.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { RoutingModule } from '../app.routes';
-import { FormsModule } from '@angular/forms';
-import { HttpModule, RequestOptions } from '@angular/http';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatMenuModule, MatCardModule, MatToolbarModule, MatIconModule, MatTableModule, MatExpansionModule, MatSnackBarModule, MatInputModule, MatFormFieldModule, MatDialogModule, MatSelectModule, MatOptionModule, MatRadioModule, MatCheckboxModule, MatTabsModule } from '@angular/material';
-import { CdkTableModule } from '@angular/cdk/table';
-import { TableModule } from 'primeng/table';
-import { FileUploadModule, SharedModule, PanelModule, RadioButtonModule, MessageModule } from 'primeng/primeng';
-import { MarkdownModule } from 'ngx-md';
-import { ToastyModule } from 'ng2-toasty';
-import { MainComponent } from '../main/main.component';
-import { HomeComponent } from '../main/home/home.component';
-import { AboutComponent } from '../main/about/about.component';
-import { DatasetinfoComponent } from '../main/datasetinfo/datasetinfo.component';
-import { RegisterComponent } from '../main/register/register.component';
-import { HomeFaqComponent } from '../main/faq/faq.component';
-import { AccountHomeComponent } from './accounthome/accounthome.component';
-import { DatasetsComponent } from './datasets/datasets.component';
-import { ExportRequestsComponent } from './exportrequests/exportrequests.component';
-import { WorkstationComponent } from './workstation/workstation.component';
-import { FaqComponent } from './faq/faq.component';
+import { Routes } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
 import { CognitoService } from '../../services/cognito.service';
 import { ApiGatewayService } from '../../services/apigateway.service';
-import * as AWS from "aws-sdk-mock";
-import { Observable } from 'rxjs/Observable';
-import { analyzeAndValidateNgModules } from '@angular/compiler';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { of } from 'rxjs/observable/of';
+import { RouterTestingModule } from '@angular/router/testing';
+
 
 describe('AccountComponent', () => {
   let component: AccountComponent;
-  //let fixture: ComponentFixture<AccountComponent>;
-  let fixture: ComponentFixture<AccountComponent>, service: CognitoService, apiService: ApiGatewayService;
-  //let mockAPIService: ApiGatewayService;
-  let mockService;
-  let mockAPIService;
+  let fixture: ComponentFixture<AccountComponent>;  
+  let mockObjectMap = {};
+
+  let RESPONSE = {
+     'username': 'testuser',
+     'email': 'testuser@gmail.com',
+     'stacks': [{ application: 'Python', instance_id: 'i-1234abc', team_bucket_name: 'dot-sdc-softwares' }],
+     'datasets': [{exportWorkflow: 'testuser@gmail.com'}],
+     'roles': 'user',
+     'userTrustedStatus': { status: 'success' }
+  }
+ 
+  const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: AccountComponent },
+    { path: '**', redirectTo: 'home' }
+  ];
 
   beforeEach(async(() => {
 
-    mockService = {
-      getUserPool: jasmine.createSpy('getUserPool')
+    let mockCognitoService = {
+      logout: jasmine.createSpy('logout')
     }
+    
+    let mockApiGatewayService = jasmine.createSpyObj(ApiGatewayService.name, {'getUserInfo': of(RESPONSE)});
 
-    //mockAPIService = {
-    //  getUserInfo: jasmine.createSpy('getUserInfo'),
-    //  userLogout: jasmine.createSpy('userLogut')
-    //}
+    mockObjectMap = Object.assign({}, {
+      mockCognitoService,
+      mockApiGatewayService
+    });
 
-    apiService = jasmine.createSpyObj('apiService', ['getUserInfo']);
-    //apiService.getUserInfo('user').and.returnValue('bob');;
-    //spyOn(mockAPIService, 'getUserInfo').and.returnValue(Promise.resolve(new Observable<{}>()));
-
+    
     TestBed.configureTestingModule({
       imports: [
-        BrowserModule,
-        RouterModule,
-        RoutingModule,
-        FormsModule,
-        HttpModule,
-        HttpClientModule,
-        BrowserAnimationsModule,
-        MatButtonModule,
-        MatMenuModule,
-        MatCardModule,
-        MatToolbarModule,
-        MatIconModule,
-        MatTableModule,
-        MatExpansionModule,
-        MatSnackBarModule,
-        MatInputModule,
-        MatFormFieldModule,
-        MatDialogModule,
-        MatSelectModule,
-        MatOptionModule,
-        MatRadioModule,
-        MatCheckboxModule,
-        MatTabsModule,
-        CdkTableModule,
-        TableModule,
-        FileUploadModule,
-        SharedModule,
-        PanelModule,
-        RadioButtonModule,
-        MessageModule,
-        MarkdownModule.forRoot(),
-        ToastyModule.forRoot(),],
-      declarations: [
-        AccountComponent,
-        MainComponent,
-        HomeComponent,
-        AboutComponent,
-        DatasetinfoComponent,
-        RegisterComponent,
-        HomeFaqComponent,
-        AccountHomeComponent,
-        DatasetsComponent,
-        ExportRequestsComponent,
-        WorkstationComponent,
-        FaqComponent],
+        RouterTestingModule.withRoutes(routes)
+      ],
+      declarations: [ AccountComponent ],
       providers: [
           {provide: APP_BASE_HREF, useValue: '/'},
-          {provide: CognitoService//, useValue: mockService
-          },
-          {provide: ApiGatewayService, useValue: apiService
-          }        
-        ]
+          {provide: CognitoService, useValue: mockCognitoService},
+          {provide: ApiGatewayService, useValue: mockApiGatewayService} 
+        ],
+        schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
 
-  
-
   beforeEach(() => {
-    //TODO 
-    // 1. figure out how to get the observable to return the response object below.
-    // 2. fill in the response object with data for the test.
-    // 3. figure out how to mock the sessionStorage to validate that the data from the response gets written into the sessionStorage
-    // let response = {
-    //   'username': null,
-    //   'email': null,
-    //   'stacks': null,
-    //   'datasets': null,
-    //   'roles': null,
-    //   'userTrustedStatus': null
-    // };
-    // let observable = new Observable();
-    //response = Observable.create();
+    let store = {};
+    const mockSessionStorage = {
+    getItem: (key: string): string => {
+      return key in store ? store[key] : null;
+    },
+    setItem: (key: string, value: string) => {
+      store[key] = `${value}`;
+    },
+    removeItem: (key: string) => {
+      delete store[key];
+    },
+    clear: () => {
+      store = {};
+    }
+       
+    };
+    spyOn(sessionStorage, 'getItem').and.callFake(mockSessionStorage.getItem);
+    spyOn(sessionStorage, 'setItem').and.callFake(mockSessionStorage.setItem);
+    spyOn(sessionStorage, 'removeItem').and.callFake(mockSessionStorage.removeItem);
+    spyOn(sessionStorage, 'clear').and.callFake(mockSessionStorage.clear);    
+    
+    let localStore = {};
+    const mockLocalStorage = {
+      clear: () => {
+        localStore = {};
+      }
+    };
 
-    //spyOn(mockAPIService, 'getUserInfo').and.returnValue(observable);
+    spyOn(localStorage, 'clear').and.callFake(mockLocalStorage.clear); 
+
     fixture = TestBed.createComponent(AccountComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it("tracks that the spy was called", function() {
-    expect(apiService.getUserInfo).toHaveBeenCalled();
-  });
-
-  it('should create', () => {
-    component.getUserInfo();
+  it('should create', () => {    
     expect(component).toBeTruthy();
   });
 
-  it('should call getUserInfo', inject([ApiGatewayService], (service: ApiGatewayService) => {  
-    component.getUserInfo();
-    spyOn(service, 'getUserInfo');
-    expect(service).toBeTruthy();
-  }));
+  it('should call getUserInfo', () => {
+    component.ngOnInit();
+    expect(mockObjectMap['mockApiGatewayService'].getUserInfo).toHaveBeenCalled();    
+   });
 
-  it('should call user logout', inject([CognitoService], (service: CognitoService) => { 
-    service.logout;   
-    spyOn(service, 'logout');
-    expect(service).toBeTruthy();
-  }));
-
+  it('should call logout', () => {
+    component.ngOnInit();
+    component.userLogout();
+    expect(localStorage.clear).toHaveBeenCalled();
+    expect(sessionStorage.clear).toHaveBeenCalled();
+    expect(mockObjectMap['mockCognitoService'].logout).toHaveBeenCalled();    
+   });
   
 });
