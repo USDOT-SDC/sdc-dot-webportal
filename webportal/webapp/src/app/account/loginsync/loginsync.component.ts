429a6878277b2b76c1a99cd8d16d358ab2094072
Tianna Woodson <tianna24@vt.edu>
2019-12-04 09:18:19 -0500

    basic sign in page that does nothing

--- webportal/webapp/src/app/account/loginsync/loginsync.component.ts
+++ webportal/webapp/src/app/account/loginsync/loginsync.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-loginsync',
   templateUrl: './loginsync.component.html',
-  styleUrls: ['./loginsync.component.css']
+  styleUrls: ['./loginsync.component.css',
+              '../../../../node_modules/uswds/src/stylesheets/uswds.scss']
 })
 export class LoginSyncComponent implements OnInit {
 
