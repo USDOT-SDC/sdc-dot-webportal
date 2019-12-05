2ae744b9e77886620b66958a2abd81a673019695
Tianna Woodson <tianna24@vt.edu>
2019-12-05 13:18:04 -0500

    now  take out env

--- webportal/webapp/src/environments/environment.ts
+++ webportal/webapp/src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: '',
   STREAMING_URL: '',
-  REGION : 'us-east-1', // User pool AWS region
-  USER_POOL_ID : 'us-east-1_Y5JI7ysvY' , // User pool ID
-  CLIENT_ID : '207smrvtpcd6ucoup3s7ph0lb0', //p client ID
+  REGION : '', // User pool AWS region
+  USER_POOL_ID : '' ,// User pool ID
+  CLIENT_ID :'', //p client ID
   IDENTITY_PROVIDER : '', // User pool Identity provider name
-  APP_DOMAIN : 'dev-sdc-dot-webportal', // App domain name
-  REDIRECT_URL : 'http://localhost:4200/index.html', // Re-direct URL for the user pool
+  APP_DOMAIN :'', // App domain name
+  REDIRECT_URL : '', // Re-direct URL for the user pool
   API_ENDPOINT : ''// AWS API gateway base endpoint
 };
