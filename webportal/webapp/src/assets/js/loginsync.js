035ebcec9c38753c9c3dbd06c6f5dc53d3fd2c8c
Tianna Woodson <tianna24@vt.edu>
2019-12-05 08:35:28 -0500

    remove js

--- webportal/webapp/src/assets/js/loginsync.js
+++
@@ -1,4 +0,0 @@
-function togglePassword(e1, e2) {
-  e2.type === "password" ? e2.type = "text" : e2.type = "password";
-  e2.type === "password" ? e1.innerHTML = "Show password" : e1.innerHTML = "Hide password";
-}
