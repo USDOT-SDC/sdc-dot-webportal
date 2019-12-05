3011b7af8768bbbe8f29a17a6fcb20d619321f25
Tianna Woodson <tianna24@vt.edu>
2019-12-05 09:13:42 -0500

    test the directive

--- webportal/webapp/src/app/account/loginsync/togglepassword.directive.spec.ts
+++ webportal/webapp/src/app/account/loginsync/togglepassword.directive.spec.ts
@@ -1,8 +1,39 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { Component } from '@angular/core';
 import { TogglePasswordDirective } from './togglepassword.directive';
 
-describe('TogglepasswordDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TogglePasswordDirective();
-    expect(directive).toBeTruthy();
+@Component({
+  template: '<input type="password" appTogglePassword>'
+})
+class TestComponent {
+  constructor() {}
+}
+
+describe('TogglePasswordDirective', () => {
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestComponent, TogglePasswordDirective]
+    });
+
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeDefined();
+  });
+
+  it('should toggle to show password when clicked once', () => {
+    const debugEl: HTMLElement = fixture.debugElement.nativeElement;
+    const input: HTMLElement = debugEl.querySelector('input');
+    const aTag: HTMLElement = debugEl.querySelector('a');
+
+    aTag.click();
+    fixture.detectChanges();
+
+    expect(input.getAttribute('type')).toEqual('text');
   });
 });
