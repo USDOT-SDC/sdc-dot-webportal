058b6c851b9f5b0e6df83588e13746badcb54f04
Tianna Woodson <tianna24@vt.edu>
2019-12-10 15:01:36 -0500

    add auth token to httpOptions

--- webportal/webapp/src/app/account/loginsync/services/loginsyncservice.service.ts
+++ webportal/webapp/src/app/account/loginsync/services/loginsyncservice.service.ts
@@ -3,23 +3,27 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
 import { environment } from '../../../../environments/environment';
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json'
-  })
-};
+import { CognitoService } from '../../../../services/cognito.service';
 
 @Injectable()
 export class LoginSyncService {
+  httpOptions = {};
   env = environment.production === 'true' ? 'prod' : 'dev';
   linkAccountUrl = `https://aae0n1czsf.execute-api.us-east-1.amazonaws.com/${this.env}/${this.env}-link-account`;
   accountLinkedUrl = `https://aae0n1czsf.execute-api.us-east-1.amazonaws.com/${this.env}/${this.env}-account-linked`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cognitoService: CognitoService) {
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': this.cognitoService.getIdToken(),
+        'Access-Control-Allow-Origin': '*'
+      })
+    };
+  }
 
   userAccountsLinked(): Observable<any> {
-    return this.http.get(this.accountLinkedUrl)
+    return this.http.get(this.accountLinkedUrl, this.httpOptions)
       .map((response) => {
         return Observable.throw(response);
       }).catch(this.handleError);
@@ -31,7 +35,7 @@ export class LoginSyncService {
       'password': password
     };
 
-    return this.http.post(this.linkAccountUrl, payload, httpOptions)
+    return this.http.post(this.linkAccountUrl, payload, this.httpOptions)
       .map((response) => {
         return Observable.throw(response);
       }).catch(this.handleError);
