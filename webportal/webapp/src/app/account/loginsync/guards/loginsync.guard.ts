edbc514ac139628672da5eb7374ce0917d45b8e1
Tianna Woodson <tianna24@vt.edu>
2019-12-11 07:59:45 -0500

    fix loginsync guard

--- webportal/webapp/src/app/account/loginsync/guards/loginsync.guard.ts
+++ webportal/webapp/src/app/account/loginsync/guards/loginsync.guard.ts
@@ -1,37 +1,24 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { LoginSyncService } from '../services/loginsyncservice.service';
+import { of } from 'rxjs/observable/of';
 
 @Injectable()
 export class LoginSyncGuard implements CanActivate {
-  accountsLinked = false;
-
   constructor(private loginSyncService: LoginSyncService, private router: Router) { }
 
   canActivate(): Observable<boolean> | Promise<boolean> | boolean {
-      /*
-        {
-          'statusCode': 200,
-          'body': { 'accountLinked': true }
-        }
-      */
-     console.log('before');
-      this.loginSyncService
-          .userAccountsLinked()
-          .subscribe(
-            result => { 
-              console.log('inside');
-              console.log(result);
-              this.accountsLinked = result.value.accountLinked; },
-            error => {
-              console.log(error);
-            });
-      console.log('after');
-      if (this.accountsLinked) {
-        return true; // Continue to account/accounthome
-      }
-      this.router.navigate(['account/loginsync']); // Redirect to the /loginsync page if the user signed in with Login.gov creds
-      return false;
+    return this.loginSyncService.userAccountsLinked()
+                                .map(response => {
+                                  if (response.value['accountLinked']) {
+                                    return true; // Continue to the /accounthome page
+                                  }
+                                  this.router.navigate(['account/loginsync']); // Navigate to the /loginsync page
+                                  return false;
+                                }).catch(error => {
+                                  console.log('error', error);
+                                  return of(false);
+                                });
   }
 }
