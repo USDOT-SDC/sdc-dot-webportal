de28176dc9a74ac1bea6ad122dddc783e80262f7
Tianna Woodson <tianna24@vt.edu>
2019-12-11 11:21:48 -0500

    sike

--- webportal/webapp/src/environments/environment.ts
+++ webportal/webapp/src/environments/environment.ts
@@ -7,10 +7,11 @@ export const environment = {
   production: '',
   STREAMING_URL: '',
   REGION : '', // User pool AWS region
-  USER_POOL_ID : '' ,// User pool ID
-  CLIENT_ID :'', //p client ID
+  USER_POOL_ID : '' , // User pool ID
+  CLIENT_ID : '', //p client ID
   IDENTITY_PROVIDER : '', // User pool Identity provider name
-  APP_DOMAIN :'', // App domain name
+  APP_DOMAIN : '', // App domain name
   REDIRECT_URL : '', // Re-direct URL for the user pool
-  API_ENDPOINT : ''// AWS API gateway base endpoint
+  API_ENDPOINT : '', // AWS API gateway base endpoint
+  LOGIN_GOV_COGNITO_APP_CLIENT_ID: '' // AWS Cognito client id
 };
