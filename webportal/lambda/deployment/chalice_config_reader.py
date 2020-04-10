07f406d6d9b25795da02be957b7422a775645581
Aaron Franssell <aaron.franssell@gmail.com>
2020-04-10 14:52:43 -0400

    transfer

--- webportal/lambda/deployment/chalice_config_reader.py
+++ webportal/lambda/deployment/chalice_config_reader.py
@@ -5,7 +5,7 @@ def find_deployed_config(resource_type, environment, deployment_path='.chalice/d
     with open(f'{deployment_path}{environment}.json') as f:
         deployed_json = json.load(f)
 
-    return next(config for config in deployed_json['resources'] if config['resource_type'] == resource_type)
+    return next((config for config in deployed_json['resources'] if config['resource_type'] == resource_type), None)
 
 
 def chalice_config(path='.chalice/config.json'):
--- webportal/lambda/deployment/cli_parser.py
+++ webportal/lambda/deployment/cli_parser.py
@@ -5,6 +5,6 @@ VALID_ENVIRONMENTS = ['dev', 'test', 'prod', 'production']
 
 def parse_arguments(args):
     parser = argparse.ArgumentParser(description='Deploys the Webportal Lambda to a specified environment')
-    parser.add_argument('--environment', choices=VALID_ENVIRONMENTS)
+    parser.add_argument('--environment', choices=VALID_ENVIRONMENTS, required=True)
     result = parser.parse_args(args)
     return result
