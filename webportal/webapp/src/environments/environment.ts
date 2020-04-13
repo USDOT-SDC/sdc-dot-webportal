082d6e3e2747c7bc8d3783a258ede0ebdc7e826a
Reena Patel <reena.patel@icf.com>
2020-04-13 09:12:54 -0400

    login reset -- part 1

--- webportal/webapp/src/environments/environment.ts
+++ webportal/webapp/src/environments/environment.ts
@@ -2,21 +2,21 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-export const environment = {
-  production: '',
-  STREAMING_URL: '',
-  REGION : '', // User pool AWS region
-  USER_POOL_ID : '' , // User pool ID
-  CLIENT_ID : '', // User pool client ID
-  IDENTITY_PROVIDER : '', // User pool Identity provider name
-  APP_DOMAIN : '', // App domain name
-  REDIRECT_URL : '', // Re-direct URL for the user pool
-  API_ENDPOINT : '', // AWS API gateway base endpoint
-  LOGIN_GOV_COGNITO_APP_CLIENT_ID: '', // AWS Cognito client id
-  LOGIN_GOV_ACCOUNT_LINK_URL: '', // API endpoint for link-account
-};
-/*
-Sample values for development:
+// export const environment = {
+//   production: '',
+//   STREAMING_URL: '',
+//   REGION : '', // User pool AWS region
+//   USER_POOL_ID : '' , // User pool ID
+//   CLIENT_ID : '', // User pool client ID
+//   IDENTITY_PROVIDER : '', // User pool Identity provider name
+//   APP_DOMAIN : '', // App domain name
+//   REDIRECT_URL : '', // Re-direct URL for the user pool
+//   API_ENDPOINT : '', // AWS API gateway base endpoint
+//   LOGIN_GOV_COGNITO_APP_CLIENT_ID: '', // AWS Cognito client id
+//   LOGIN_GOV_ACCOUNT_LINK_URL: '', // API endpoint for link-account
+// };
+
+// Sample values for development:
 export const environment = {
   production: false,
   STREAMING_URL: 'https://dev-stream.securedatacommons.com/guacamole/?authToken=',
@@ -30,4 +30,3 @@ export const environment = {
   LOGIN_GOV_COGNITO_APP_CLIENT_ID: '3ikuiqen3fdfl5brs5uk49cvsu',
   LOGIN_GOV_ACCOUNT_LINK_URL: 'https://yr5qma7k4m.execute-api.us-east-1.amazonaws.com',
 };
-*/
