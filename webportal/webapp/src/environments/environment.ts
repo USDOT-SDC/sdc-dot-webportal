85efbd2d98dce9a46160d1615497c0992d7cde13
Aaron Franssell <aaron.franssell@gmail.com>
2020-04-15 12:00:43 -0400

    Fix DoT Login (#65)
    * diff
    
    * revert

--- webportal/webapp/src/environments/environment.ts
+++ webportal/webapp/src/environments/environment.ts
@@ -8,11 +8,11 @@ export const environment = {
   STREAMING_URL: 'https://dev-stream.securedatacommons.com/guacamole/?authToken=',
   REGION: 'us-east-1',
   USER_POOL_ID: 'us-east-1_Y5JI7ysvY',
-  CLIENT_ID: '6bqivipmgp9aqbsvqjghlenhjg',
+  CLIENT_ID: '7apdoeuudp6606blne6cstpneu',
   IDENTITY_PROVIDER: 'dev-dot-ad',
   APP_DOMAIN: 'dev-sdc-dot-webportal',
   REDIRECT_URL: 'https://dev-portal.securedatacommons.com/index.html', //dev environment
-  // REDIRECT_URL: http://localhost:4200/index.html', // local development
+  // REDIRECT_URL: 'http://localhost:4200/index.html', // local development
   API_ENDPOINT: 'https://u2zksemc1h.execute-api.us-east-1.amazonaws.com/api/',
   LOGIN_GOV_COGNITO_APP_CLIENT_ID: '3ikuiqen3fdfl5brs5uk49cvsu',
   LOGIN_GOV_ACCOUNT_LINK_URL: 'https://dhclxz0yoa.execute-api.us-east-1.amazonaws.com',
