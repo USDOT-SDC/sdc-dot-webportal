36dde8d59b66bc91025ce68a2204b6ca07e9274d
Tianna Woodson <tianna24@vt.edu>
2020-04-23 09:07:51 -0400

    Revert Password Reset Feature Updates (#72)
    * move alert and loader back to loginsync components folder
    
    * checkout master some files
    
    * delete passwordreset folder
    
    * move loader files back to loginsync services folder
    
    * checkout more files from master

--- webportal/webapp/src/app/app.module.ts
+++ webportal/webapp/src/app/app.module.ts
@@ -36,14 +36,14 @@ import {RadioButtonModule} from 'primeng/primeng';
 import { ExportRequestsComponent } from './account/exportrequests/exportrequests.component';
 import {MessageModule} from 'primeng/message';
 import { LoginSyncComponent } from './account/loginsync/components/loginsync.component';
-import { AlertComponent } from './account/components/alert/alert.component';
+import { AlertComponent } from './account/loginsync/components/alert/alert.component';
 import { TogglePasswordDirective } from './account/loginsync/directives/togglepassword.directive';
 import { LoginSyncGuard } from './account/loginsync/guards/loginsync.guard';
 import { LoginSyncService } from './account/loginsync/services/loginsyncservice.service';
-import { LoaderComponent } from './account/components/loader/loader.component';
-import { LoaderService } from './account/services/loader.service';
-import { LoaderInterceptor } from './account/services/loader.interceptor';
-import { PasswordResetComponent } from './account/passwordreset/passwordreset.component'
+import { LoaderComponent } from './account/loginsync/components/loader/loader.component';
+import { LoaderService } from './account/loginsync/services/loader.service';
+import { LoaderInterceptor } from './account/loginsync/services/loader.interceptor';
+
 
 @NgModule({
   declarations: [
@@ -65,7 +65,6 @@ import { PasswordResetComponent } from './account/passwordreset/passwordreset.co
     AlertComponent,
     TogglePasswordDirective,
     LoaderComponent,
-    PasswordResetComponent
   ],
   imports: [
     BrowserModule,
