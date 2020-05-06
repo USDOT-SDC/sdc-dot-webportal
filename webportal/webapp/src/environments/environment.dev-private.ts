29bd7ffd99a957285bba2db1f6c29b22a0bc15fa
Justin Spencer <justin.spencer@icf.com>
2020-05-06 17:24:13 -0400

    Updating client ID

--- webportal/webapp/src/environments/environment.dev-private.ts
+++ webportal/webapp/src/environments/environment.dev-private.ts
@@ -3,7 +3,7 @@ export const environment = {
   STREAMING_URL: 'https://dev-stream.securedatacommons.com/guacamole/?authToken=',
   REGION: 'us-east-1',
   USER_POOL_ID: 'us-east-1_Y5JI7ysvY',
-  CLIENT_ID: '7apdoeuudp6606blne6cstpneu',
+  CLIENT_ID: '22gr1lfintehcaac2cdmmsur52',
   IDENTITY_PROVIDER: 'dev-dot-ad',
   APP_DOMAIN: 'dev-sdc-dot-webportal',
   REDIRECT_URL: 'https://dev-proxy-portal.securedatacommons.com/index.html', //dev environment
