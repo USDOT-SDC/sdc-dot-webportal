fc8125f769ae6391e5cf45d6407f9871f7034abe
Justin Spencer <justin.spencer@icf.com>
2020-05-27 15:49:58 -0400

    Env variable adjustments
    Hold onto the old URL variable til we do cleanup, and default to ADFS provider

--- webportal/webapp/src/environments/environment.ts
+++ webportal/webapp/src/environments/environment.ts
@@ -8,13 +8,14 @@ export const environment = {
   STREAMING_URL: 'https://dev-stream.securedatacommons.com/guacamole/?authToken=',
   REGION: 'us-east-1',
   USER_POOL_ID: 'us-east-1_Y5JI7ysvY',
-  CLIENT_ID: '22gr1lfintehcaac2cdmmsur52',
-  IDENTITY_PROVIDER: 'dev-dot-ad',
+  CLIENT_ID: '207smrvtpcd6ucoup3s7ph0lb0',
+  IDENTITY_PROVIDER: 'USDOT-ADFS',
   APP_DOMAIN: 'dev-sdc-dot-webportal',
   REDIRECT_URL: 'https://dev-portal.securedatacommons.com/index.html', //dev environment
   //REDIRECT_URL: 'http://localhost:4200/index.html', // local development
   API_ENDPOINT: 'https://u2zksemc1h.execute-api.us-east-1.amazonaws.com/api/',
   LOGIN_GOV_COGNITO_APP_CLIENT_ID: '3ikuiqen3fdfl5brs5uk49cvsu',
+  LOGIN_GOV_ACCOUNT_LINK_URL: 'https://dhclxz0yoa.execute-api.us-east-1.amazonaws.com',
   ACCOUNT_LINK_URL: 'https://dhclxz0yoa.execute-api.us-east-1.amazonaws.com/dev',
   LINK_ACCOUNT_PATH: 'dev-link-account',
   ACCOUNT_LINKED_PATH: 'dev-account-linked',
