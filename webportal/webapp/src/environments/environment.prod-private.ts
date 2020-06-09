80b5ac0031452a70f13a4bec936f5aca9e1026a0
Justin Spencer <justin.spencer@icf.com>
2020-06-09 12:56:54 -0400

    Adding production-private and updating script
    Updating the prod script to push into the private bucket. Also removing a duplicate test config in the angular JSON

---
+++ webportal/webapp/src/environments/environment.prod-private.ts
@@ -0,0 +1,17 @@
+// Sample prod values below:
+export const environment = {
+  production: true,
+  STREAMING_URL: 'https://stream.securedatacommons.com/guacamole/?authToken=',
+  REGION: 'us-east-1',
+  USER_POOL_ID: 'us-east-1_1zLqP2XrI',
+  CLIENT_ID: 'gt6jl7jeqogmelu5tm7ce1vnb',
+  IDENTITY_PROVIDER: 'USDOT-ADFS',
+  APP_DOMAIN: 'prod-sdc-dot-webportal',
+  REDIRECT_URL: 'https://proxy-portal.securedatacommons.com/index.html',
+  API_ENDPOINT: 'https://proxy-portal.securedatacommons.com/webportal-api/',
+  LOGIN_GOV_COGNITO_APP_CLIENT_ID: '3mlku34f58oeloqqc3hpiko6l3',
+  ACCOUNT_LINK_URL: 'https://proxy-portal.securedatacommons.com/account-link-prod',
+  LINK_ACCOUNT_PATH: 'prod-link-account',
+  ACCOUNT_LINKED_PATH: 'prod-account-linked',
+  RESET_TEMPORARY_PASSWORD_PATH: 'prod-reset-temporary-password'
+};
