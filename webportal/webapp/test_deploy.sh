99dc52402a9fbe64ef02c9cb6771bf01f4b09c2a
Lallan Yadav <Lallan.Yadav@icf.com>
2020-06-10 17:41:44 -0400

    Add Test env configuration for Web portal
    Add Test env configuration for Web portal,
    Need to validate properties file for test-private ts.

--- webportal/webapp/test_deploy.sh
+++ webportal/webapp/test_deploy.sh
@@ -1,6 +1,19 @@
 #/bin/bash
-ng build --aot=true --configuration=test
-#Copy everything over to S3 bucket
-aws s3 cp --profile sdc ./dist s3://test-dot-sdc-webportal-hosting --recursive --metadata-directive REPLACE --cache-control max-age=86400 --acl public-read
-#Bust open the cache
-aws s3 cp --profile sdc ./dist/index.html s3://test-dot-sdc-webportal-hosting/index.html --region us-east-1 --metadata-directive REPLACE --cache-control max-age=0 --acl public-read
+
+echo "***"
+echo "Building assets..."
+ng build --configuration=test-private
+
+
+# Use distinct test-private bucket so we can modify URLs, etc. per the environment
+# Copy everything over to S3 bucket
+echo "Copying to s3..."
+aws s3 cp --profile sdc ./dist s3://test-private-sdc-webportal-hosting --recursive --only-show-errors
+
+# Refresh assets on nginx proxies
+echo "Refreshing assets on proxies..."
+aws ssm send-command --profile sdc \
+  --document-name test-nginx-asset-update \
+  --parameters staticAssetsBucket="test-private-sdc-webportal-hosting" \
+  --targets "Key=tag:Name,Values=test-nginx-web-proxy" \
+  --comment "Deploying sdc-dot-webportal to test at $(date) and refreshing assets"
