5323afe6315fabcdbad9cd261aae706c487d56e4
Lallan Yadav <Lallan.Yadav@icf.com>
2020-06-17 15:12:01 -0400

    Update Login.gov Logins in PROD Through Proxied Login.gov APIs and Proxy Guacamole
    Update Login.gov Logins in PROD Through Proxied Login.gov APIs and Proxy Guacamole

--- webportal/webapp/src/environments/environment.prod-private.ts
+++ webportal/webapp/src/environments/environment.prod-private.ts
@@ -1,7 +1,7 @@
 // Sample prod values below:
 export const environment = {
   production: true,
-  STREAMING_URL: 'https://stream.securedatacommons.com/guacamole/?authToken=',
+  STREAMING_URL: 'https://portal.securedatacommons.com/guacamole/?authToken=',
   REGION: 'us-east-1',
   USER_POOL_ID: 'us-east-1_1zLqP2XrI',
   CLIENT_ID: 'gt6jl7jeqogmelu5tm7ce1vnb',
@@ -9,7 +9,7 @@ export const environment = {
   APP_DOMAIN: 'prod-sdc-dot-webportal',
   REDIRECT_URL: 'https://portal.securedatacommons.com/index.html',
   API_ENDPOINT: 'https://portal.securedatacommons.com/webportal-api/',
-  LOGIN_GOV_COGNITO_APP_CLIENT_ID: '3mlku34f58oeloqqc3hpiko6l3',
+  LOGIN_GOV_COGNITO_APP_CLIENT_ID: '5s27prc70o5sj84u2ksq7n8opo',
   ACCOUNT_LINK_URL: 'https://portal.securedatacommons.com/account-link-prod',
   LINK_ACCOUNT_PATH: 'prod-link-account-private',
   ACCOUNT_LINKED_PATH: 'prod-account-linked-private',
