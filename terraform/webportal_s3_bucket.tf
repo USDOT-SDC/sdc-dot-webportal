7e0cf8c6e6df520ab8d44bdcb560c62b67307aac
Aaron Franssell <aaron.franssell@gmail.com>
2020-09-24 15:46:20 -0400

    encrypt!

--- terraform/webportal_s3_bucket.tf
+++ terraform/webportal_s3_bucket.tf
@@ -2,6 +2,13 @@ resource "aws_s3_bucket" "webportal_bucket" {
   bucket        = var.webportal_bucket_name
   acl           = "private"
   force_destroy = false
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 
   tags = local.global_tags
 }
