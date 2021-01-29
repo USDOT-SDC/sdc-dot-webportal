7f4d53b56e235ea143554a32972dfb1ca1e36c27
Hannah Kent <hannah.kent.ctr@ad.dot.gov>
2021-01-29 11:21:59 -0500

    changed bucket to original

--- terraform/config/ecs-dev.tfvars
+++ terraform/config/ecs-dev.tfvars
@@ -1,3 +1,3 @@
 deploy_env = "dev"
-webportal_bucket_name = "gov.dot.sdc.dev-portal"
+webportal_bucket_name = "dev-webportal-hosting-505135622787"
 lambda_trigger_buckets = [ ]
--- webportal/webapp/ecs_dev_deploy.sh
+++ webportal/webapp/ecs_dev_deploy.sh
@@ -7,12 +7,12 @@ ng build --configuration=ecs-dev
 
 # Copy everything over to S3 bucket
 echo "Copying to s3..."
-aws s3 cp ./dist s3://gov.dot.sdc.dev-portal --recursive --only-show-errors
+aws s3 cp ./dist s3://dev-webportal-hosting-505135622787 --recursive --only-show-errors
 
 # Refresh assets on nginx proxies
 echo "Refreshing assets on proxies..."
 aws ssm send-command --region us-east-1 \
   --document-name dev-nginx-asset-update \
-  --parameters staticAssetsBucket="gov.dot.sdc.dev-portal" \
+  --parameters staticAssetsBucket="dev-webportal-hosting-505135622787" \
   --targets "Key=tag:Name,Values=dev-nginx-web-proxy" \
   --comment "Deploying sdc-dot-webportal to dev at $(date) and refreshing assets"
