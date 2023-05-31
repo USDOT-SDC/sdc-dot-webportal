38cae13a486c8359209ae76fddcaa184d2f6c025
williamrsharp <69211914+williamrsharp@users.noreply.github.com>
2023-05-31 16:38:42 +0000

    Started functionality of connecting form fields to variables in the ts file

--- webportal/webapp/src/app/main/datasetinfo/datasetinfo.component.ts
+++ webportal/webapp/src/app/main/datasetinfo/datasetinfo.component.ts
@@ -9,27 +9,50 @@ import { FieldsetModule } from 'primeng/fieldset';
 import { PanelModule } from 'primeng/panel';
 import { DialogModule } from 'primeng/dialog';
 import { CheckboxModule } from 'primeng/checkbox';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, MatCardModule, ButtonModule, CheckboxModule, DialogModule, FormsModule, InputTextModule, InputTextareaModule, FieldsetModule, PanelModule, ],
+  imports: [CommonModule, MatCardModule, ReactiveFormsModule, ButtonModule, CheckboxModule, DialogModule, FormsModule, InputTextModule, InputTextareaModule, FieldsetModule, PanelModule, ],
   selector: 'app-datasetinfo',
   templateUrl: './datasetinfo.component.html',
   styleUrls: ['./datasetinfo.component.css']
 })
 export class DatasetinfoComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
   displayDialog = false;
   isNoticeChecked: boolean = false;
+  form: FormGroup;
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      name: new FormControl('', Validators.required),
+      // Add other form controls here
+    });
+  }
+
+
+
+  
 
   showDialog() {
     this.displayDialog = true;
+  }
+
+  submitForm() {
+    if (this.form.valid) {
+      // Form is valid, you can access the form values here
+      const name = this.form.value.name;
+      // Access other form values as needed
+
+      // Perform any desired logic with the form data
+      // For example, you could send the form data to an API
+      console.log('Form submitted');
+    } else {
+      // Form is not valid, handle the error or provide user feedback
+      console.log('Form is not valid');
+    }
   }
 
 }
