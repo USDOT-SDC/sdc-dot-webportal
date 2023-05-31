580e1de859b1256aa48a8e2c74338e41d4ad072a
williamrsharp <69211914+williamrsharp@users.noreply.github.com>
2023-05-31 16:13:29 +0000

    Creating new HSIS form

--- webportal/webapp/src/app/main/datasetinfo/datasetinfo.component.ts
+++ webportal/webapp/src/app/main/datasetinfo/datasetinfo.component.ts
@@ -1,10 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { FieldsetModule } from 'primeng/fieldset';
+import { PanelModule } from 'primeng/panel';
+import { DialogModule } from 'primeng/dialog';
+import { CheckboxModule } from 'primeng/checkbox';
+
 
 @Component({
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, ButtonModule, CheckboxModule, DialogModule, FormsModule, InputTextModule, InputTextareaModule, FieldsetModule, PanelModule, ],
   selector: 'app-datasetinfo',
   templateUrl: './datasetinfo.component.html',
   styleUrls: ['./datasetinfo.component.css']
@@ -14,6 +23,13 @@ export class DatasetinfoComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  displayDialog = false;
+  isNoticeChecked: boolean = false;
+
+  showDialog() {
+    this.displayDialog = true;
   }
 
 }
