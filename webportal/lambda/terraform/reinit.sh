9c3641e63a76d27d5efe47fdbbbfbaa5a4547f91
Jeff Ussing <Jeff.Ussing.CTR@dot.gov>
2023-11-08 16:36:24 -0500

    remove unused terraform

--- webportal/lambda/terraform/reinit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "${AWS_ENVIRONMENT}.sdc.dot.gov.platform.terraform"
-terraform init -var-file=config/backend-ecs-${AWS_ENVIRONMENT}.tfvars
-
--- webportal/lambda/terraform/replan.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-terraform plan -var-file=config/ecs-${AWS_ENVIRONMENT}.tfvars
-
