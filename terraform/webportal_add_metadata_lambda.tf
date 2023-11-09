7084a6e08881474fa0169ea56f37014d70a055ba
Jeff Ussing <Jeff.Ussing.CTR@dot.gov>
2023-11-08 21:35:53 -0500

    replaced s3_object with archive_file for lambda code

--- terraform/webportal_add_metadata_lambda.tf
+++ terraform/webportal_add_metadata_lambda.tf
@@ -1,43 +1,48 @@
+data "archive_file" "add_metadata" {
+  type        = "zip"
+  source_file = "${path.module}./add-metadata-to-s3-object/add_metadata.py"
+  output_path = "${path.module}./add-metadata-to-s3-object/deployment-package.zip"
+}
+
 resource "aws_lambda_function" "add_metadata" {
-  s3_bucket         = local.terraform_bucket
-  s3_key            = "sdc-dot-webportal/add-metadata-to-s3-object.zip"
-  function_name     = "${local.environment}-${var.lambda_name}"
-  role              = aws_iam_role.LambdaRole.arn
-  handler           = "add_metadata.lambda_handler"
-  source_code_hash  = base64sha256(timestamp()) # Bust cache of deployment... we want a fresh deployment everytime Terraform runs for now...
-  runtime           = "python3.7"
-  timeout           = 300
-  tags              = local.global_tags
+  function_name    = "${local.environment}-${var.lambda_name}"
+  filename         = data.archive_file.add_metadata.output_path
+  source_code_hash = data.archive_file.add_metadata.output_base64sha256
+  role             = aws_iam_role.LambdaRole.arn
+  handler          = "add_metadata.lambda_handler"
+  runtime          = "python3.11"
+  timeout          = 300
+  tags             = local.global_tags
   environment {
     variables = {
-      ENVIRONMENT                       = "${local.environment}"
-      EXPORT_REQUEST_FOLDER             = "${var.EXPORT_REQUEST_FOLDER}"
+      ENVIRONMENT           = "${local.environment}"
+      EXPORT_REQUEST_FOLDER = "${var.EXPORT_REQUEST_FOLDER}"
     }
   }
 }
 
 resource "aws_iam_role" "LambdaRole" {
-    name = "${local.environment}-${var.lambda_name}"
-    assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
+  name = "${local.environment}-${var.lambda_name}"
+  assume_role_policy = jsonencode(
     {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "arn:aws:iam::${local.account_id}:root",
-        "Service": "lambda.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Principal" : {
+            "AWS" : "arn:aws:iam::${local.account_id}:root",
+            "Service" : "lambda.amazonaws.com"
+          },
+          "Action" : "sts:AssumeRole"
+        }
+      ]
     }
-  ]
-}
-  EOF
+  )
 }
 
 data "aws_s3_bucket" "lambda_trigger_buckets" {
-  count   = length(local.lambda_trigger_buckets)
-  bucket  = local.lambda_trigger_buckets[count.index]
+  count  = length(local.lambda_trigger_buckets)
+  bucket = local.lambda_trigger_buckets[count.index]
 }
 
 data "aws_iam_policy_document" "policy_doc" {
@@ -79,7 +84,7 @@ data "aws_iam_policy_document" "policy_doc" {
   }
 
   statement {
-     effect = "Allow"
+    effect = "Allow"
 
     actions = [
       "s3:GetObject",
@@ -89,19 +94,19 @@ data "aws_iam_policy_document" "policy_doc" {
     ]
 
     resources = [
-      for id in local.lambda_trigger_buckets[*]:
-        "arn:aws:s3:::${id}/*"
-    ]   
+      for id in local.lambda_trigger_buckets[*] :
+      "arn:aws:s3:::${id}/*"
+    ]
   }
 }
 
 resource "aws_iam_policy" "LambdaPermissions" {
-    name    = "${local.environment}-${var.lambda_name}-permissions"
-    policy  = "${data.aws_iam_policy_document.policy_doc.json}"
+  name   = "${local.environment}-${var.lambda_name}-permissions"
+  policy = data.aws_iam_policy_document.policy_doc.json
 }
 
 resource "aws_iam_role_policy_attachment" "CloudWatchLogsAttachment" {
-  role = aws_iam_role.LambdaRole.name
+  role       = aws_iam_role.LambdaRole.name
   policy_arn = aws_iam_policy.LambdaPermissions.arn
 }
 
