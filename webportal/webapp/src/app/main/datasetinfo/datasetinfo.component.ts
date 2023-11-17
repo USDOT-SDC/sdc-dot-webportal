b1876721d6f1f5a3dae441d76f78bd4c597b467a
williamrsharp <williamrsharp@icloud.com>
2023-11-17 20:43:20 +0000

    Purged dead feature HSIS Data Request Form

--- webportal/webapp/src/app/main/datasetinfo/datasetinfo.component.ts
+++ webportal/webapp/src/app/main/datasetinfo/datasetinfo.component.ts
@@ -7,7 +7,7 @@ import * as $ from "jquery";
 import { Injectable, Inject } from "@angular/core";
 import { MatCardModule } from "@angular/material/card";
 import { MatExpansionModule } from "@angular/material/expansion";
-import { CommonModule } from "@angular/common";
+
 import { TableModule } from "primeng/table";
 import { RouterModule } from "@angular/router";
 import { ButtonModule } from "primeng/button";
@@ -37,8 +37,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { DropdownModule } from "primeng/dropdown";
 
 import { MatDatepickerModule } from "@angular/material/datepicker";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatExpansionModule } from "@angular/material/expansion";
+
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatIconModule } from "@angular/material/icon";
 import {
@@ -49,20 +48,16 @@ import {
 import { MatMenuModule } from "@angular/material/menu";
 import { MatSortModule } from "@angular/material/sort";
 
-import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
-import { DialogModule } from "primeng/dialog";
+
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatRadioModule } from "@angular/material/radio";
-import { MatSelectModule } from "@angular/material/select";
-import { MatSnackBarModule } from "@angular/material/snack-bar";
+
 import { MatTableModule } from "@angular/material/table";
-import { MatTabsModule } from "@angular/material/tabs";
-import { MatToolbarModule } from "@angular/material/toolbar";
+
 import { CardModule } from "primeng/card";
 import { FileUploadModule } from "primeng/fileupload";
 import { RadioButtonModule } from "primeng/radiobutton";
-import { TableModule } from "primeng/table";
 
 @Component({
   standalone: true,
@@ -96,7 +91,6 @@ import { TableModule } from "primeng/table";
     MatProgressSpinnerModule,
     MatExpansionModule,
     MatDialogModule,
-    MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
     MatFormFieldModule,
@@ -109,7 +103,6 @@ import { TableModule } from "primeng/table";
     ButtonModule,
     InputTextModule,
     FormsModule,
-    MarkdownModule,
     RouterModule,
     // MatButtonModule,
     // MatCheckboxModule,
@@ -147,111 +140,111 @@ export class DatasetinfoComponent implements OnInit {
   isFormVisible = false;
 
   ngOnInit() {
-    this.mailType == "Dataset export Mail";
-    this.form = new FormGroup({
-      name: new FormControl("", Validators.required),
-      address1: new FormControl("", Validators.required),
-      address2: new FormControl(""),
-      city: new FormControl("", Validators.required),
-      state: new FormControl("", Validators.required),
-      zipcode: new FormControl("", Validators.required),
-      phone: new FormControl("", Validators.required),
-      fax: new FormControl(""),
-      email: new FormControl("", [Validators.required, Validators.email]),
-      confirm_email: new FormControl("", [
-        Validators.required,
-        Validators.email,
-      ]),
-      // Add other form controls here
-    });
+    // this.mailType == "Dataset export Mail";
+    // this.form = new FormGroup({
+    //   name: new FormControl("", Validators.required),
+    //   address1: new FormControl("", Validators.required),
+    //   address2: new FormControl(""),
+    //   city: new FormControl("", Validators.required),
+    //   state: new FormControl("", Validators.required),
+    //   zipcode: new FormControl("", Validators.required),
+    //   phone: new FormControl("", Validators.required),
+    //   fax: new FormControl(""),
+    //   email: new FormControl("", [Validators.required, Validators.email]),
+    //   confirm_email: new FormControl("", [
+    //     Validators.required,
+    //     Validators.email,
+    //   ]),
+    //   // Add other form controls here
+    // });
   }
 
-  displayDialog: boolean = false;
-  showDatalakeDropdown: boolean = false;
-  checklistItems: any[] = [
-    { name: "Team Bucket", checked: false },
-    { name: "Datalake", checked: false },
-  ];
+  //   displayDialog: boolean = false;
+  //   showDatalakeDropdown: boolean = false;
+  //   checklistItems: any[] = [
+  //     { name: "Team Bucket", checked: false },
+  //     { name: "Datalake", checked: false },
+  //   ];
 
-  datalakeOptions: any[] = [
-    { name: "2022", checked: false },
-    { name: "2023", checked: false },
-  ];
-  fileUploaded: boolean = false; // Add this variable
+  //   datalakeOptions: any[] = [
+  //     { name: "2022", checked: false },
+  //     { name: "2023", checked: false },
+  //   ];
+  //   fileUploaded: boolean = false; // Add this variable
 
-  showDialog() {
-    this.displayDialog = true;
-  }
+  //   showDialog() {
+  //     this.displayDialog = true;
+  //   }
 
-  onFileUpload(event: any) {
-    // Handle file upload here
-    console.log("Uploaded File:", event.files[0]);
-    this.fileUploaded = true; // Set to true after file upload
-  }
+  //   onFileUpload(event: any) {
+  //     // Handle file upload here
+  //     console.log("Uploaded File:", event.files[0]);
+  //     this.fileUploaded = true; // Set to true after file upload
+  //   }
 
-  goBack() {
-    this.fileUploaded = false; // Set to false to go back to file upload section
-  }
+  //   goBack() {
+  //     this.fileUploaded = false; // Set to false to go back to file upload section
+  //   }
 
-  saveCheckedItems() {
-    const checkedItems = this.checklistItems.filter((item) => item.checked);
-    const datalakeSelected = this.checklistItems.find(
-      (item) => item.name === "Datalake"
-    )?.checked;
+  //   saveCheckedItems() {
+  //     const checkedItems = this.checklistItems.filter((item) => item.checked);
+  //     const datalakeSelected = this.checklistItems.find(
+  //       (item) => item.name === "Datalake"
+  //     )?.checked;
 
-    // Logic to save the checked items
-    console.log("Checked items:", checkedItems);
-    if (datalakeSelected) {
-      const datalakeExtraOptions = this.datalakeOptions.filter(
-        (option) => option.checked
-      );
-      console.log("Datalake Extra Options:", datalakeExtraOptions);
-    }
+  //     // Logic to save the checked items
+  //     console.log("Checked items:", checkedItems);
+  //     if (datalakeSelected) {
+  //       const datalakeExtraOptions = this.datalakeOptions.filter(
+  //         (option) => option.checked
+  //       );
+  //       console.log("Datalake Extra Options:", datalakeExtraOptions);
+  //     }
 
-    this.displayDialog = false;
-  }
+  //     this.displayDialog = false;
+  //   }
 
-  closeDialog() {
-    this.displayDialog = false;
-    this.showDatalakeDropdown = false;
-    this.datalakeOptions.forEach((option) => (option.checked = false));
-  }
+  //   closeDialog() {
+  //     this.displayDialog = false;
+  //     this.showDatalakeDropdown = false;
+  //     this.datalakeOptions.forEach((option) => (option.checked = false));
+  //   }
 
-  submitForm() {
-    if (this.form.valid) {
-      // Form is valid, you can access the form values here
-      const name = this.form.value.name;
-      const address1 = this.form.value.address1;
-      const address2 = this.form.value.address2;
-      const city = this.form.value.city;
-      const state = this.form.value.state;
-      const zipcode = this.form.value.zipcode;
-      const phone = this.form.value.phone;
-      const fax = this.form.value.fax;
-      const email = this.form.value.email;
-      const confirmEmail = this.form.value.confirm_email;
-      // Access other form values as needed
+  //   submitForm() {
+  //     if (this.form.valid) {
+  //       // Form is valid, you can access the form values here
+  //       const name = this.form.value.name;
+  //       const address1 = this.form.value.address1;
+  //       const address2 = this.form.value.address2;
+  //       const city = this.form.value.city;
+  //       const state = this.form.value.state;
+  //       const zipcode = this.form.value.zipcode;
+  //       const phone = this.form.value.phone;
+  //       const fax = this.form.value.fax;
+  //       const email = this.form.value.email;
+  //       const confirmEmail = this.form.value.confirm_email;
+  //       // Access other form values as needed
 
-      console.log(name);
+  //       console.log(name);
 
-      this.messageService.add({
-        severity: "success",
-        summary: "Success",
-        detail: "Data Request Form Submitted",
-      });
+  //       this.messageService.add({
+  //         severity: "success",
+  //         summary: "Success",
+  //         detail: "Data Request Form Submitted",
+  //       });
 
-      this.isFormVisible = false;
-      // Perform any desired logic with the form data
-      // For example, you could send the form data to an API
-      console.log("Form submitted");
-    } else {
-      // Form is not valid, handle the error or provide user feedback
-      this.messageService.add({
-        severity: "error",
-        summary: "Error",
-        detail: "Please fill in all required fields",
-      });
-      console.log("Form is not valid");
-    }
-  }
+  //       this.isFormVisible = false;
+  //       // Perform any desired logic with the form data
+  //       // For example, you could send the form data to an API
+  //       console.log("Form submitted");
+  //     } else {
+  //       // Form is not valid, handle the error or provide user feedback
+  //       this.messageService.add({
+  //         severity: "error",
+  //         summary: "Error",
+  //         detail: "Please fill in all required fields",
+  //       });
+  //       console.log("Form is not valid");
+  //     }
+  //   }
 }
